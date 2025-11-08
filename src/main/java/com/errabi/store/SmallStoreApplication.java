aea63f51a3b89770eb69cc179387377bdbed05ed
errabi ayoub <errabi.ayoub@gmail.com>
2025-11-08 23:38:29 +0100

    Trigger ci

--- src/main/java/com/errabi/store/SmallStoreApplication.java
+++ src/main/java/com/errabi/store/SmallStoreApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StoreApplication {
+public class SmallStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
+		SpringApplication.run(SmallStoreApplication.class, args);
 	}
 
 }
