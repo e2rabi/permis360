770607d2d6c32e2e585b57a0adac91f16de521e8
errabi ayoub <errabi.ayoub@gmail.com>
2025-11-09 12:56:06 +0100

    refactor - Setup project build plugins

--- modules/customer-core/src/main/java/ma/errabi/store/CustomerCoreApplication.java
+++ modules/customer-core/src/main/java/ma/errabi/store/CustomerCoreApplication.java
@@ -1,13 +1,13 @@
-package com.errabi.store;
+package ma.errabi.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmallStoreApplication {
+public class CustomerCoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmallStoreApplication.class, args);
+		SpringApplication.run(CustomerCoreApplication.class, args);
 	}
 
 }
