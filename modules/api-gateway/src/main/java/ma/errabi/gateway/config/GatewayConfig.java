2fbb35442e6b9ba9848c2bfdf03282a7875b6c76
errabi ayoub <errabi.ayoub@gmail.com>
2026-06-05 12:22:39 +0100

    Add API Gateway service

---
+++ modules/api-gateway/src/main/java/ma/errabi/gateway/config/GatewayConfig.java
@@ -0,0 +1,17 @@
+package ma.errabi.gateway.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class GatewayConfig {
+
+   /* @Autowired
+    private ReactorLoadBalancerExchangeFilterFunction lbFunction;
+    @Bean
+    public WebClient getLoadBalanceClient(WebClient.Builder builder) {
+        return builder.filter(lbFunction).build();
+    }*/
+
+}
